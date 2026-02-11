bddb2052e3d7cc6e76d3bf9e2578d3b25444fcfc
VandeCedan <nmaxies@yahoo.fr>
2026-02-11 11:59:02 +0100

    Render not found fix

--- rxconfig.py
+++ rxconfig.py
@@ -3,6 +3,6 @@ import reflex as rx
 
 config = rx.Config(
     app_name="budget_erp",
-    db_url="sqlite:///reflex.db",
+    db_url=os.getenv("DATABASE_URL", "sqlite:///reflex.db"),
     api_url=os.getenv("API_URL", "http://localhost:8000"),
 )
